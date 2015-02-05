d4c553a75a1919a59def60fdeccaf6e511bbd9c6
wenber <wenbo.fe@gmail.com>
2015-02-05 18:35:58 +0800

    新增ajaxSession文件，引入ajax会话流概念

---
+++ src/ajaxSession.js
@@ -0,0 +1,25 @@
+/**
+ * @file ajaxSession,用于串联一组完成的ajax会话
+ * @author Ming Liu(liuming07@baidu.com)
+ */
+
+define(function (require) {
+    var fc = require('fc-core');
+    /**
+     * ajaxSession 全局ajaxSession对象
+     * 每一个键值对标识一个会话流
+     * @type {Object}
+     * example:
+     * {
+     *     sessionId: eventId,
+     *     sessionId: eventId,
+     *     sessionId: eventId
+     * }
+     */
+    var ajaxSession = ajaxSession || {};
+
+    return {
+        session: ajaxSession,
+        createSessionId: fc.util.guid
+    };
+});
